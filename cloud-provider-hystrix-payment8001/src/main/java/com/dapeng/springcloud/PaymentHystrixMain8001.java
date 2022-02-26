1755bb4a793e9d316136bb8196b260dda9033b4d
pg-dpw <55357364+pg-dpw@users.noreply.github.com>
2022-02-26 16:16:44 +0800

    hystrix服务监控

--- cloud-provider-hystrix-payment8001/src/main/java/com/dapeng/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/dapeng/springcloud/PaymentHystrixMain8001.java
@@ -1,10 +1,12 @@
 package com.dapeng.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,5 +15,25 @@ public class PaymentHystrixMain8001 {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class,args);
+    }
+
+    /**
+     * 方法描述: 此配置是为了服务监控而配置，与服务容错无关，springcloud升级后的坑，
+     * ServletRegistrationBean因为Springboot的默认路径不是/hystrix.stream
+     * 只要在自己的项目里配置上下面的servlet就可以了。不然会报404 ：
+     * Unable to connect to Command Metric Stream
+     * @param:  * @param null
+     * @return: {@link null}
+     * @author: diaopengwei
+     * @date: 2022/2/26
+     */
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
--- cloud-provider-hystrix-payment8001/src/main/java/com/dapeng/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/dapeng/springcloud/controller/PaymentController.java
@@ -1,13 +1,14 @@
 package com.dapeng.springcloud.controller;
 
 import com.dapeng.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-
+@Slf4j
 @RestController
 public class PaymentController {
 
@@ -28,6 +29,15 @@ public class PaymentController {
     public String paymentInfo_Timeout (@PathVariable("id") Integer id ) {
         String result = paymentService.paymentInfo_Timeout( id );
         System.out.println(" result--- " + result);
+        return result;
+    }
+
+    //===服务熔断
+    //===服务熔断
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("*******result:"+result);
         return result;
     }
 
