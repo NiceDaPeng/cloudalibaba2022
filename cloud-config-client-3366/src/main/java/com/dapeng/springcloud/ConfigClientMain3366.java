51e30acc5e2579c40870c98dfe6f704e6df7f55c
pg-dpw <55357364+pg-dpw@users.noreply.github.com>
2022-02-28 16:20:55 +0800

    springcloud Bus +rabbitMQ 实现动态获取配置。实现一次通知，处处修改

---
+++ cloud-config-client-3366/src/main/java/com/dapeng/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,14 @@
+package com.dapeng.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ConfigClientMain3366.class,args );
+    }
+}
